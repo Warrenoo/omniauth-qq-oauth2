635971c0f4b4218a275de34daef815d6d070d0f6
Warren Shen <541991a@gmail.com>
2015-07-02 16:29:01 +0800

    Update qq_connect.rb

--- lib/omniauth/strategies/qq_connect.rb
+++ lib/omniauth/strategies/qq_connect.rb
@@ -5,7 +5,7 @@ require 'omniauth/strategies/oauth2'
 module OmniAuth
   module Strategies
     class QQConnect < OmniAuth::Strategies::OAuth2
-      option :name, "qq_connect"
+      option :name, "qqt"
 
       option :client_options, {
         :site => 'https://graph.qq.com/oauth2.0/',
